61c1247f96c4e143a0083dbcfe7e6604003589a6
dellream <yakimov.as@mail.ru>
2023-08-12 14:37:49 +0500

    Change config files

--- app/db/database_connect.py
+++ app/db/database_connect.py
@@ -1,12 +1,18 @@
-from sqlalchemy import create_engine
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 from app.config import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
 
-# Формируем строку для подключения
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Формируем URL для подключения
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Создаем асинхронную сессию
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker as session:
+        yield session
--- migrations/env.py
+++ migrations/env.py
@@ -5,13 +5,20 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASSWORD
 from app.models.domain.menus_models import metadata
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
 section = config.config_ini_section
+config.set_section_option(section, "DB_HOST", DB_HOST)
+config.set_section_option(section, "DB_PORT", DB_PORT)
+config.set_section_option(section, "DB_USER", DB_USER)
+config.set_section_option(section, "DB_NAME", DB_NAME)
+config.set_section_option(section, "DB_PASSWORD", DB_PASSWORD)
 
 
 # Interpret the config file for Python logging.
