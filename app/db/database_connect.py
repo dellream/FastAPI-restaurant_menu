483f3727daa003297497a87e9a2427126ee05da2
dellream <yakimov.as@mail.ru>
2023-08-12 10:18:18 +0500

    Removed synchronous part

--- app/db/database_connect.py
+++ app/db/database_connect.py
@@ -9,10 +9,4 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-# Dependency to get the database session
-def get_db():
-    session = SessionLocal()
-    try:
-        yield session
-    finally:
-        session.close()
+
